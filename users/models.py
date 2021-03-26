faf5ec1493b5bd1e81bed8eedb94aa2052e38e3e
yessm621 <yessm621@gmail.com>
2021-03-26 21:50:34 +0900

    make faker user, amenities, facilities

--- users/models.py
+++ users/models.py
@@ -24,7 +24,7 @@ class User(AbstractUser):
     CURRENCY_USD = "usd"
     CURRENCY_KRW = "krw"
 
-    CURRENCY_CHOICES = ((CURRENCY_USD, "usd"), (CURRENCY_KRW, "krw"))
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
     avatar = models.ImageField(upload_to="avatars", blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
