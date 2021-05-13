b6c4e33a102db6a3c93a7b7b809732ea2bbd9d65
yessm621 <yessm621@gmail.com>
2021-05-13 15:51:28 +0900

    forms modify

--- users/forms.py
+++ users/forms.py
@@ -27,9 +27,9 @@ class SignUpForm(forms.ModelForm):
         model = models.User
         fields = ("first_name", "last_name", "email")
         widgets = {
-            'first_name': forms.TextInput(attrs={"placeholder": "First Name"}),
-            'last_name': forms.TextInput(attrs={"placeholder": "Last Name"}),
-            'email': forms.EmailInput(attrs={"placeholder": "Email"})
+            "first_name": forms.TextInput(attrs={"placeholder": "First Name"}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Last Name"}),
+            "email": forms.EmailInput(attrs={"placeholder": "Email Name"}),
         }
 
     password = forms.CharField(
@@ -39,19 +39,28 @@ class SignUpForm(forms.ModelForm):
         widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"})
     )
 
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError(
+                "That email is already taken", code="existing_user"
+            )
+        except models.User.DoesNotExist:
+            return email
+
     def clean_password1(self):
         password = self.cleaned_data.get("password")
         password1 = self.cleaned_data.get("password1")
-
         if password != password1:
             raise forms.ValidationError("Password confirmation does not match")
         else:
             return password
 
-    def save(self, *args, **kwargw):
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
         email = self.cleaned_data.get("email")
         password = self.cleaned_data.get("password")
-        user = super().save(commit=False)
         user.username = email
         user.set_password(password)
         user.save()
