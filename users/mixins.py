76faadc75f00c3b8801e50e2596334d08ff29ff2
yessm621 <yessm621@gmail.com>
2021-07-13 11:21:00 +0900

    room detail

--- users/mixins.py
+++ users/mixins.py
@@ -1,5 +1,16 @@
-from django.shortcuts import redirect, reverse
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+
+class EmailLoginOnlyView(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.login_method == "email"
+
+    def handle_no_permission(self):
+        messages.error(self.request, "Can't go there")
+        return redirect("core:home")
 
 
 class LoggedOutOnlyView(UserPassesTestMixin):
@@ -10,4 +21,9 @@ class LoggedOutOnlyView(UserPassesTestMixin):
         return not self.request.user.is_authenticated
 
     def handle_no_permission(self):
+        messages.error(self.request, "Can't go there")
         return redirect("core:home")
+
+
+class LoggedInOnlyView(LoginRequiredMixin):
+    login_url = reverse_lazy("users:login")
